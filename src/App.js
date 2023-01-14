e0ef94d4fe212ddfc1b25545ac279d031f9639a8
Joel Taylor <jtayped@gmail.com>
2023-01-14 16:44:43 +0100

    styling changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Timer, Tasks } from "./containers/index";
-import { Player } from "./components/index";
+import { Player, Socials } from "./components/index";
 
 function App() {
   return (
@@ -8,6 +8,7 @@ function App() {
       <Player />
       <Timer workTime={1500} restTime={500} pomodorosTillLongRest={4} />
       <Tasks />
+      <Socials />
     </div>
   );
 }
